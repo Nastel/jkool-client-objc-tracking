4237bd944a71b8b09badf6cf7876cd073bcf98fa
Nastel <cbernardone@nastel.com>
2016-09-27 12:29:42 -0400

    Finished adding second screen. Began adding code to detect if on wifi or 3G. Also began adding code to determine IP address.

---
+++ Example/jkool-client-objc-tracking/jkViewController2.h
@@ -0,0 +1,22 @@
+//
+//  jkViewController.h
+//  jkool-client-objc-tracking
+//
+//  Created by jKool developer on 09/20/2016.
+//  Copyright (c) 2016 jKool developer. All rights reserved.
+//
+
+@import UIKit;
+
+@interface jkViewController2 : UIViewController
+{
+    IBOutlet UIButton *button1;
+    IBOutlet UIButton *button2;
+    IBOutlet UIButton *button3;
+}
+
+- (IBAction)buttonOneClick:(id)sender;
+- (IBAction)buttonTwoClick:(id)sender;
+- (IBAction)buttonThreeClick:(id)sender;
+
+@end
---
+++ Example/jkool-client-objc-tracking/jkViewController2.m
@@ -0,0 +1,42 @@
+//
+//  jkViewController.m
+//  jkool-client-objc-tracking
+//
+//  Created by jKool developer on 09/20/2016.
+//  Copyright (c) 2016 jKool developer. All rights reserved.
+//
+
+#import "jkViewController2.h"
+
+@interface jkViewController2 ()
+
+@end
+
+@implementation jkViewController2
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)buttonOneClick:(id)sender {
+    
+}
+
+- (IBAction)buttonTwoClick:(id)sender {
+    
+}
+
+- (IBAction)buttonThreeClick:(id)sender {
+    
+}
+
+
+
+@end
